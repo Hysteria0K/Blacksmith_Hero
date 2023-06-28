8db207675a6bd36acffa7d962b5aabe709ae2a4e
Hysteria0K <tksthqo123@gmail.com>
2023-06-28 14:15:19 +0900

    충돌 판정 생성

---
+++ Blacksmith_Hero/Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float Enemy_Speed;
+    private float Col_Speed;
+    public bool Col_check;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Col_Speed = 500.0f;
+        Col_check = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Col_check == true)
+        {
+            Col_Speed -= 1.0f;
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(Col_Speed, 0.0f);
+        }
+
+        else
+        {
+            this.transform.Translate(Vector2.left * Enemy_Speed);
+        }
+
+        if (Col_Speed <= 0.0f)
+        {
+            Col_check = false;
+            Col_Speed = 0.0f;
+        }
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Ãæµ¹");
+            Col_Speed = 500.0f;
+            Col_check = true;
+        }
+    }
+}
+        
