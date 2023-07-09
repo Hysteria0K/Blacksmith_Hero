9a74c5b0f56b660ada6297020c1f01cd218a1026
Hysteria0K <tksthqo123@gmail.com>
2023-07-10 08:05:32 +0900

    일단 복사 성공함

--- Blacksmith_Hero/Assets/Scripts/EventSystem.cs
+++ Blacksmith_Hero/Assets/Scripts/EventSystem.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class EventSystem : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         //화면 설정
         Screen.SetResolution(720, 1280, true);
@@ -15,16 +15,18 @@ public class EventSystem : MonoBehaviour
 
 
         // 파일 생성
-        if (Application.isMobilePlatform) //Application.isMobilePlatform
-        {
-            string filePath = Path.Combine(Application.persistentDataPath, "DataTable");
 
-            if (!File.Exists(filePath))
-            {
-                File.WriteAllText(filePath, "복사기가 안되는거임");
-                // 파일이 존재하지 않으면 새로 생성
-                CopyStreamingAssetsToPersistentDataPath();
-            }
+        string filePath = Path.Combine(Application.persistentDataPath, "/DataTable");
+
+        if (!File.Exists(filePath))
+        {
+            Debug.Log(filePath);
+            YourMethodName();
+        }
+
+        else
+        {
+            Debug.Log("복사불필요");
         }
     }
 
@@ -34,35 +36,63 @@ public class EventSystem : MonoBehaviour
 
     }
 
-    private void CopyStreamingAssetsToPersistentDataPath()
+    private void YourMethodName()
     {
-        string streamingAssetsPath = Application.streamingAssetsPath;
-        string persistentDataPath = Application.persistentDataPath;
-
-        CopyFolderContents(streamingAssetsPath, persistentDataPath);
+        CopyFilesToPersistentDataPath();
     }
 
-    private void CopyFolderContents(string sourceFolder, string destinationFolder)
+    private void CopyFilesToPersistentDataPath()
     {
-        if (!Directory.Exists(destinationFolder))
+        string sourcePath = Application.streamingAssetsPath + "/DataTable";
+        string targetPath = Application.persistentDataPath + "/DataTable";
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+    // Android에서는 WWW 클래스를 사용하여 파일에 접근해야 합니다.
+    using (UnityWebRequest www = UnityWebRequest.Get(sourcePath))
+    {
+        www.SendWebRequest();
+
+        while (!www.isDone)
         {
-            Directory.CreateDirectory(destinationFolder);
+            // 대기
         }
 
-        string[] files = Directory.GetFiles(sourceFolder);
+        if (www.result == UnityWebRequest.Result.Success)
+        {
+            Directory.CreateDirectory(targetPath);
+            string[] files = Directory.GetFiles(sourcePath);
+
+            foreach (string filePath in files)
+            {
+                string fileName = Path.GetFileName(filePath);
+                string targetFilePath = Path.Combine(targetPath, fileName);
+
+                if (!File.Exists(targetFilePath))
+                {
+                    File.WriteAllBytes(targetFilePath, www.downloadHandler.data);
+                }
+            }
+        }
+    }
+#else
+        // Android 이외의 플랫폼에서는 일반적인 파일 복사 메서드를 사용합니다.
+        if (!Directory.Exists(targetPath))
+        {
+            Directory.CreateDirectory(targetPath);
+        }
+
+        string[] files = Directory.GetFiles(sourcePath);
+
         foreach (string filePath in files)
         {
             string fileName = Path.GetFileName(filePath);
-            string destinationPath = Path.Combine(destinationFolder, fileName);
-            File.Copy(filePath, destinationPath, true);
-        }
+            string targetFilePath = Path.Combine(targetPath, fileName);
 
-        string[] subfolders = Directory.GetDirectories(sourceFolder);
-        foreach (string subfolderPath in subfolders)
-        {
-            string folderName = Path.GetFileName(subfolderPath);
-            string destinationSubfolder = Path.Combine(destinationFolder, folderName);
-            CopyFolderContents(subfolderPath, destinationSubfolder);
+            if (!File.Exists(targetFilePath))
+            {
+                File.Copy(filePath, targetFilePath);
+            }
         }
+#endif
     }
 }
--- Blacksmith_Hero/Assets/Scripts/Status_Reader.cs
+++ Blacksmith_Hero/Assets/Scripts/Status_Reader.cs
@@ -13,7 +13,6 @@ public class Status_Reader : MonoBehaviour
     public int Gold;
     public int Token;
 
-
     public string Enemy_Name;
     public int Enemy_Hp;
     public int Enemy_Atk;
@@ -22,24 +21,25 @@ public class Status_Reader : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         List<Dictionary<string, object>> Player_Status = CSVReader.Read("Player_Status.csv");
         List<Dictionary<string, object>> Enemy_Status = CSVReader.Read("Enemy_Status.csv");
-        List<Dictionary<string, object>> DataBase= CSVReader.Read("DataBase.csv");
+        List<Dictionary<string, object>> DataBase = CSVReader.Read("DataBase.csv");
 
         if (DataBase[0].ContainsKey("Stage")) if (int.TryParse(DataBase[0]["Stage"].ToString(), out Stage)) { }
         if (DataBase[0].ContainsKey("Gold")) if (int.TryParse(DataBase[0]["Gold"].ToString(), out Gold)) { }
         if (DataBase[0].ContainsKey("Token")) if (int.TryParse(DataBase[0]["Token"].ToString(), out Token)) { }
 
-        if (Player_Status[Player_Level-1].ContainsKey("Hp")) if (int.TryParse(Player_Status[Player_Level-1]["Hp"].ToString(), out Player_Hp)) { }
-        if (Player_Status[Player_Level-1].ContainsKey("Atk")) if (int.TryParse(Player_Status[Player_Level-1]["Atk"].ToString(), out Player_Atk)) { }
+        if (Player_Status[Player_Level - 1].ContainsKey("Hp")) if (int.TryParse(Player_Status[Player_Level - 1]["Hp"].ToString(), out Player_Hp)) { }
+        if (Player_Status[Player_Level - 1].ContainsKey("Atk")) if (int.TryParse(Player_Status[Player_Level - 1]["Atk"].ToString(), out Player_Atk)) { }
 
         Enemy_Name = (string)Enemy_Status[Stage - 1]["Name"];
 
         if (Enemy_Status[Stage - 1].ContainsKey("Hp")) if (int.TryParse(Enemy_Status[Stage - 1]["Hp"].ToString(), out Enemy_Hp)) { }
         if (Enemy_Status[Stage - 1].ContainsKey("Atk")) if (int.TryParse(Enemy_Status[Stage - 1]["Atk"].ToString(), out Enemy_Atk)) { }
         if (Enemy_Status[Stage - 1].ContainsKey("Gold")) if (int.TryParse(Enemy_Status[Stage - 1]["Gold"].ToString(), out Enemy_Gold)) { }
-                                
-    }
+    }          
+    
 
     // Update is called once per frame
     void Update()
