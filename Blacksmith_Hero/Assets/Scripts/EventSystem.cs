2ea6a457e2f10383e4c67e06ec75cd6433d0b1ec
Hysteria0K <tksthqo123@gmail.com>
2023-07-10 08:29:00 +0900

    복사 막기 성공,,, 하지만 여전히 안드로이드에서는 복사 안됨

--- Blacksmith_Hero/Assets/Scripts/EventSystem.cs
+++ Blacksmith_Hero/Assets/Scripts/EventSystem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class EventSystem : MonoBehaviour
 {
@@ -16,12 +17,12 @@ public class EventSystem : MonoBehaviour
 
         // 파일 생성
 
-        string filePath = Path.Combine(Application.persistentDataPath, "/DataTable");
+        string filePath = Path.Combine(Application.persistentDataPath, "DataTable");
 
-        if (!File.Exists(filePath))
+        if (!Directory.Exists(filePath))
         {
             Debug.Log(filePath);
-            YourMethodName();
+            CopyFilesToPersistentDataPath();
         }
 
         else
@@ -36,11 +37,6 @@ public class EventSystem : MonoBehaviour
 
     }
 
-    private void YourMethodName()
-    {
-        CopyFilesToPersistentDataPath();
-    }
-
     private void CopyFilesToPersistentDataPath()
     {
         string sourcePath = Application.streamingAssetsPath + "/DataTable";
@@ -48,17 +44,27 @@ public class EventSystem : MonoBehaviour
 
 #if UNITY_ANDROID && !UNITY_EDITOR
     // Android에서는 WWW 클래스를 사용하여 파일에 접근해야 합니다.
+
+    string testPath1 = Path.Combine(Application.persistentDataPath, "test1.csv");
+    string testPath2 = Path.Combine(Application.persistentDataPath, "test2.csv");
+    string testPath3 = Path.Combine(Application.persistentDataPath, "test3.csv");
+    string testPath4 = Path.Combine(Application.persistentDataPath, "test4.csv");
+    string testPath5 = Path.Combine(Application.persistentDataPath, "test5.csv");
+    string testPath6 = Path.Combine(Application.persistentDataPath, "test6.csv");
     using (UnityWebRequest www = UnityWebRequest.Get(sourcePath))
     {
+        File.WriteAllText(testPath1, "test1");
         www.SendWebRequest();
 
         while (!www.isDone)
         {
             // 대기
+            File.WriteAllText(testPath2, "test2");
         }
 
         if (www.result == UnityWebRequest.Result.Success)
         {
+            File.WriteAllText(testPath3, "test3");
             Directory.CreateDirectory(targetPath);
             string[] files = Directory.GetFiles(sourcePath);
 
@@ -70,9 +76,21 @@ public class EventSystem : MonoBehaviour
                 if (!File.Exists(targetFilePath))
                 {
                     File.WriteAllBytes(targetFilePath, www.downloadHandler.data);
+                    File.WriteAllText(testPath4, "test4");
+                }
+
+                else
+                {
+                    File.WriteAllText(testPath5, "test5");
                 }
             }
         }
+
+        else
+        {
+            File.WriteAllText(testPath6, "test6");
+        }
+
     }
 #else
         // Android 이외의 플랫폼에서는 일반적인 파일 복사 메서드를 사용합니다.
