a586fdad07dd57c7fff37afad8b43a56d84376e6
Hysteria0K <tksthqo123@gmail.com>
2023-07-13 14:59:20 +0900

    Right_Wall updated

--- Blacksmith_Hero/Assets/Scripts/Next_Portal.cs
+++ Blacksmith_Hero/Assets/Scripts/Next_Portal.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Next_Portal : MonoBehaviour
 {
     public GameObject UI_Manager;
+
+    public GameObject Right_Wall;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,5 +27,11 @@ public class Next_Portal : MonoBehaviour
             UI_Manager.GetComponent<UI_Manager>().Fade();
         }
     }
-
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Right_Wall.SetActive(true);
+        }
+    }
 }
