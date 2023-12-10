3e7e4ad42279ba15771bc90f0a07b2ef1e87ed79
Hysteria0K <tksthqo123@gmail.com>
2023-12-10 22:49:14 +0900

    모듈 생성까지 완료

--- Blacksmith_Hero/Assets/Scripts/Equip_Manager.cs
+++ Blacksmith_Hero/Assets/Scripts/Equip_Manager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 
@@ -15,6 +16,32 @@ public class Equip_Manager : MonoBehaviour
     public int Equip_Status; // 실제 값
 
     private int Selected_Equip_Type; // 선택된 장비 타입
+
+    public int Module1;
+    public int Module2;
+    public int Module3;
+
+    public int Module1_Type;
+    public int Module2_Type;
+    public int Module3_Type;
+
+    public int Module1_Min_Status;
+    public int Module2_Min_Status;
+    public int Module3_Min_Status;
+
+    public int Module1_Max_Status;
+    public int Module2_Max_Status;
+    public int Module3_Max_Status;
+
+    public int Module1_Status;
+    public int Module2_Status;
+    public int Module3_Status;
+
+    private int Module1_Selected;
+    private int Module2_Selected;
+    private int Module3_Selected;
+
+    public int Item_Code;
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +60,7 @@ public class Equip_Manager : MonoBehaviour
         List<Dictionary<string, object>> Module_Data = CSVReader.Read("module_datatable.csv");
 
         if (Equip_Data[0].ContainsKey("Max_Index")) int.TryParse(Equip_Data[0]["Max_Index"].ToString(), out Equip_Max_Index);
+        if (Module_Data[0].ContainsKey("Max_Index")) int.TryParse(Module_Data[0]["Max_Index"].ToString(), out Module_Max_Index);
 
         Selected_Equip_Type = Random.Range(0, Equip_Max_Index); // 장비 결정
 
@@ -43,6 +71,39 @@ public class Equip_Manager : MonoBehaviour
 
         Equip_Status = Random.Range(Equip_Min_Status, Equip_Max_Status + 1);
 
+        Module1_Selected = Random.Range(0, Module_Max_Index);
 
+        while(true)
+        {
+            Module2_Selected = Random.Range(0, Module_Max_Index);
+            if (Module2_Selected != Module1_Selected) break;
+        }
+
+        while (true)
+        {
+            Module3_Selected = Random.Range(0, Module_Max_Index);
+            if (Module3_Selected != Module1_Selected && Module3_Selected != Module2_Selected) break;
+        }
+
+        if (Module_Data[Module1_Selected].ContainsKey("Index")) int.TryParse(Module_Data[Module1_Selected]["Index"].ToString(), out Module1);
+        if (Module_Data[Module1_Selected].ContainsKey("Type")) int.TryParse(Module_Data[Module1_Selected]["Type"].ToString(), out Module1_Type);
+        if (Module_Data[Module1_Selected].ContainsKey("Min_Stat")) int.TryParse(Module_Data[Module1_Selected]["Min_Stat"].ToString(), out Module1_Min_Status);
+        if (Module_Data[Module1_Selected].ContainsKey("Max_Stat")) int.TryParse(Module_Data[Module1_Selected]["Max_Stat"].ToString(), out Module1_Max_Status);
+
+        if (Module_Data[Module2_Selected].ContainsKey("Index")) int.TryParse(Module_Data[Module2_Selected]["Index"].ToString(), out Module2);
+        if (Module_Data[Module2_Selected].ContainsKey("Type")) int.TryParse(Module_Data[Module2_Selected]["Type"].ToString(), out Module2_Type);
+        if (Module_Data[Module2_Selected].ContainsKey("Min_Stat")) int.TryParse(Module_Data[Module2_Selected]["Min_Stat"].ToString(), out Module2_Min_Status);
+        if (Module_Data[Module2_Selected].ContainsKey("Max_Stat")) int.TryParse(Module_Data[Module2_Selected]["Max_Stat"].ToString(), out Module2_Max_Status);
+
+        if (Module_Data[Module3_Selected].ContainsKey("Index")) int.TryParse(Module_Data[Module3_Selected]["Index"].ToString(), out Module3);
+        if (Module_Data[Module3_Selected].ContainsKey("Type")) int.TryParse(Module_Data[Module3_Selected]["Type"].ToString(), out Module3_Type);
+        if (Module_Data[Module3_Selected].ContainsKey("Min_Stat")) int.TryParse(Module_Data[Module3_Selected]["Min_Stat"].ToString(), out Module3_Min_Status);
+        if (Module_Data[Module3_Selected].ContainsKey("Max_Stat")) int.TryParse(Module_Data[Module3_Selected]["Max_Stat"].ToString(), out Module3_Max_Status);
+
+        Module1_Status = Random.Range(Module1_Min_Status, Module1_Max_Status + 1);
+        Module2_Status = Random.Range(Module2_Min_Status, Module2_Max_Status + 1);
+        Module3_Status = Random.Range(Module3_Min_Status, Module3_Max_Status + 1);
     }
+
+
 }
